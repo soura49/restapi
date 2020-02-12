f747caa8b93b2dabca395eee51e83c2d265cf43d
msdhondi <balakrishnasoura@Balakrishnas-MacBook-Pro.local>
2020-02-11 19:57:18 -0500

    adding the new functionality

--- amster/postput.go
+++ amster/postput.go
@@ -18,14 +18,22 @@ const (
 	Dbname   = "titanic"
 )
 
-//InsertOperation is the Function to insert data to Database
-func InsertOperation(uuid string, p People) (string, error) {
+func dbconnection() (*sql.DB, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		Host, Port, User, Password, Dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		return " ", nil
+		return nil, err
+	}
+	return db, nil
+}
+
+//InsertOperation is the Function to insert data to Database
+func InsertOperation(uuid string, p People) (string, error) {
+	db, err := dbconnection()
+	if err != nil {
+		return " ", err
 	}
 	defer db.Close()
 	sqlstmt := `
@@ -41,4 +49,36 @@ func InsertOperation(uuid string, p People) (string, error) {
 		return " ", err
 	}
 	return "Successfully Inserted", nil
+}
+
+//SelectOperationByID reperesents the function to retrieve the data from the database based on the given uuid input
+func SelectOperationByID(uuid string) (*sql.Row, error) {
+	db, err := dbconnection()
+	if err != nil {
+		return nil, err
+	}
+	sqlstmt := `select info from people where uuid=$1`
+	defer db.Close()
+	out := db.QueryRow(sqlstmt, uuid)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+//SelectOperationAll reperesents the function to retrieve all the data from the database
+func SelectOperationAll() (*sql.Rows, error) {
+	db, err := dbconnection()
+	if err != nil {
+		return nil, err
+	}
+	sqlstmt := `
+	select * from people
+	`
+	defer db.Close()
+	out, err := db.Query(sqlstmt)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
--- main.go
+++ main.go
@@ -50,12 +50,41 @@ func updatePeople(w http.ResponseWriter, r *http.Request) {
 }
 
 func getPeople(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(pp)
+	out, err := amster.SelectOperationAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for out.Next() {
+		var uuid string
+		var info string
+		err = out.Scan(&uuid, &info)
+		if err != nil {
+			panic(err)
+		}
+		w.Write([]byte(info))
+	}
 }
+
+func getPeopleByID(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	fmt.Println(params)
+	var info string
+	out, err := amster.SelectOperationByID(params["id"])
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = out.Scan(&info)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write([]byte(info))
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/people", updatePeople).Methods("POST")
 	router.HandleFunc("/people", getPeople).Methods("GET")
+	router.HandleFunc("/people/{id}", getPeopleByID).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8088", router))
 }
